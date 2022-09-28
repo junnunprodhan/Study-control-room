7fb485dc75bc24001d765e133f54f203b42a0824
junnun15 <junnunprodhan@gmail.com>
2022-09-28 13:48:11 -0700

    responsive for mobile device

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -6,12 +6,14 @@ import UserInfo from '../UserInfo/UserInfo';
 
 const User = () => {
     return (
-        <div>
-            <UserInfo></UserInfo>
-            <h3>Add a Break Time</h3>
-            <AddBreak></AddBreak>
-            <StudyDetails></StudyDetails>
-            <button className='btn-activate'>Activity Completed</button>            
+        <div className='user-activity'>
+            <div className='user-card'>
+                <UserInfo></UserInfo>
+                <h3>Add a Break Time</h3>
+                <AddBreak></AddBreak>
+                <StudyDetails></StudyDetails>
+                <button className='btn-activate'>Activity Completed</button>  
+            </div>          
         </div>
     );
 };
